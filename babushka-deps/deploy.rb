d79a21b5a790eb012509ac5d96aa888bbccfd59c
Ben Hoskings <ben@hoskings.net>
2015-06-17 13:43:49 +0900

    Update DB schema after pushing.

--- babushka-deps/deploy.rb
+++ babushka-deps/deploy.rb
@@ -7,6 +7,10 @@ dep 'on deploy', :old_id, :new_id, :branch, :env do
   requires 'common:when path changed'.with('db/migrate/', 'conversation:migrated db', old_id, new_id, env)
 end
 
+dep 'after push', :ref, :remote, :env do
+  requires 'benhoskings:schema up to date.push'.with(ref, remote, env)
+end
+
 dep 'built', template: 'task' do
   run {
     shell "bundle exec pith -i pith/ -o public/ build", :log => true
